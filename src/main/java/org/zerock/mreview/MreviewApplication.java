91765ffd6f2521d058539e01f31757e7cac52c29
zerock <zerock@zerockui-MacBookPro.local>
2023-02-10 14:11:57 +0900

    Initial commit

---
+++ src/main/java/org/zerock/mreview/MreviewApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.mreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MreviewApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/zerock/mreview/entity/Review.java
@@ -0,0 +1,29 @@
+package org.zerock.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString(exclude = {"movie","member"})
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewnum;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    private int grade;
+
+    private String text;
+
+}
